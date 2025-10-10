e1270db781cbd3fd70217da190b454bf009ef469
PavelAlexander <alexander.sw@seznam.cz>
2025-10-10 06:31:59 +0000

    nahrání

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2==0: # % pro zbytek po dělění a zbytek je 0
+        print (f"Číslo {cislo} je sudé!") #f pro vypisování proměnné
+    else:
+        print (f"Číslo {cislo} je liché!") #podmínka není naplněna
 
+
+sudy_nebo_lichy (5) #volání funkce
+sudy_nebo_lichy(1000000)
+    
